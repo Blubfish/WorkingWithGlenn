4e5baf40a229362d5d853514eebe824ee9f5d52d
Gia Bao Ta <giabaota2020@gmail.com>
2023-10-24 10:23:26 -0500

    Initial Commit

---
+++ WorkingWithGlenn/WorkingWithGlennApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkingWithGlennApp.swift
+//  WorkingWithGlenn
+//
+//  Created by Gia Bao Ta on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkingWithGlennApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
