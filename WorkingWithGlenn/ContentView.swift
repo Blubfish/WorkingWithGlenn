f64b5caf0ca04620ddf26a6c6c993a9aa380bec6
Glenn Joy <gjoy4589@stu.d214.org>
2023-10-26 10:36:42 -0500

    code for rating system

--- WorkingWithGlenn/ContentView.swift
+++ WorkingWithGlenn/ContentView.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var foodName: String = ""
+    @State private var rating: Double = 5
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Food Name", text: $foodName)
+                }
+                Section {
+                    HStack {
+                        Text("Rating:")
+                        Spacer()
+                        Stepper(value: $rating, in: 1...10, step: 1) {
+                            Text("\(Int(rating))")
+                        }
+                    }
+                }
+                Section {
+                    Button(action: {
+                        print("Food Name: \(foodName)")
+                        print("Rating: \(rating)")
+                    }) {
+                        Text("Submit")
+                    }
+                }
+            }
+            .navigationBarTitle("Food Rating")
+        }
+    }
+}
--- WorkingWithGlenn/WorkingWithGlennApp.swift
+++ WorkingWithGlenn/WorkingWithGlennApp.swift
@@ -15,7 +15,8 @@ struct WorkingWithGlennApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+         ContentView()
+            
         }
     }
 }
